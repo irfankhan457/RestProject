59ae1d6b7432b94e9926f549b84c959945652784
irfan khan <irfankhan457@gmail.com>
2020-04-09 03:17:57 +0530

    Spring restful web service internationalization by using LocaleContextHolder.getLocale()

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -34,10 +35,11 @@ public class HelloWorldController {
 		return new HelloWorldBean(String.format("Hello World %s", name));
 	}
 	
+	
 	@GetMapping(value = "/hello-world-intrnationalized")
-	public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", 
-	required=false) Locale locale) {
-		String result = messageSource.getMessage("good.morning.message", null, locale);
+	public String helloWorldInternationalized() {
+		String result = messageSource.getMessage("good.morning.message", null, 
+				LocaleContextHolder.getLocale());
 		return result;
 	}
 }
