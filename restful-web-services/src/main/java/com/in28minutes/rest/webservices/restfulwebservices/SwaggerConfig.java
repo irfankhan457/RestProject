80812ea225be2b3583543a17cd4495389f07a78a
irfan khan <irfankhan457@gmail.com>
2020-04-14 07:44:53 +0530

    ApiInfo DEFAULT_API_INFO added for swagger api

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/SwaggerConfig.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/SwaggerConfig.java
@@ -10,6 +10,9 @@ import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,10 +20,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+	
+	public static final Contact DEFAULT_CONTACT = new Contact(
+			"Irfan Khan", "www.deliverylelo.com", "irfankhan457@gmail.com");
+	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
+			"My Awesome Api", "My Awesome Api Documentation", "1.0", "urn:tos",
+	          DEFAULT_CONTACT, "Apache 2.0", 
+	          "http://www.apache.org/licenses/LICENSE-2.0", 
+	          new ArrayList<VendorExtension>());
+
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2)
+		.apiInfo(DEFAULT_API_INFO);
 	}
 	
 	@Bean
